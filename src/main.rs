0f2e5c190523def43b49415db9c99d469b557a5e
Adam Blance <adam.blance@gmail.com>
2021-12-16 13:13:38 +0000

    Added functions for read/write

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+#![feature(bigint_helper_methods)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_parens)]
+
 use std::fs;
 
 mod opc;
+
+struct Nes {
+    cpu: Cpu,
+    wram: [u8; 2048],
+    cart: Cartridge,
+}
 
 struct Cpu {
     a: u8,
@@ -12,13 +23,95 @@ struct Cpu {
     cycles: u64,
 }
 
+enum PBit {
+    N,  // Negative
+    V,  // Overflow
+    B,  // Break
+    I,  // IRQ Disable
+    Z,  // Zero
+    C,  // Carry
+}
+
 struct Cartridge {
     prg_rom: Vec<u8>,
     chr_rom: Vec<u8>,
     mapper: u8,
 }
 
+fn main() {
 
+    let ines_data = fs::read("rom.nes").expect("Failed to read rom");
+
+    if ines_data.len() < 16 {
+        panic!();
+    }
+    if &ines_data[0..4] != b"NES\x1A" {
+        panic!();
+    }
+    
+    let prg_start: usize = 16;
+    let prg_end = prg_start + (ines_data[4] as usize) * 16384;
+    let chr_end = prg_end + (ines_data[5] as usize) * 8192;
+
+    let cart = Cartridge {
+        prg_rom: ines_data[prg_start..prg_end].to_vec(),
+        chr_rom: ines_data[prg_end..chr_end].to_vec(),
+        mapper: (ines_data[7] & 0xF0) | (ines_data[6] >> 4),
+    };
+
+
+    emulate(&cart);
+
+}
+
+fn read_three_bytes(addr: u16, wram: &[u8], cart: &Cartridge) -> (u8, u8, u8) {
+    (
+        read_mem(addr, wram, cart),
+        read_mem(addr.saturating_add(1), wram, cart),
+        read_mem(addr.saturating_add(2), wram, cart)
+    )
+}
+
+fn read_mem(addr: u16, wram: &[u8], cart: &Cartridge) -> u8 {
+    match addr {
+        0x0000..=0x1FFF => wram[(addr % 0x800) as usize],
+        0x2000..=0x3FFF => wram[(0x2000 + (addr % 0x8)) as usize],
+        0x4000..=0x401F => wram[addr as usize],
+        0x4020..=0x5FFF => wram[addr as usize],
+        0x6000..=0x7FFF => wram[addr as usize],
+        0x8000..=0xBFFF => cart.prg_rom[addr as usize],
+        0xC000..=0xFFFF => cart.prg_rom[(addr - 0x4000) as usize],
+    }
+}
+
+fn write_mem(addr: u16, val: u8, wram: &mut [u8]) {
+    if addr < 0x2000 {
+        wram[(addr % 0x800) as usize] = val;
+    }
+}
+
+fn read_p_bit(bit: PBit, cpu: &Cpu) -> bool {
+    match bit {
+        PBit::N => (cpu.p & 0b10000000) != 0,
+        PBit::V => (cpu.p & 0b01000000) != 0,
+        PBit::B => (cpu.p & 0b00010000) != 0,
+        PBit::I => (cpu.p & 0b00000100) != 0,
+        PBit::Z => (cpu.p & 0b00000010) != 0,
+        PBit::C => (cpu.p & 0b00000001) != 0,
+    }
+}
+
+fn write_c_bit(bit: PBit, val: bool, cpu: &mut Cpu) {
+    let set = val as u8;
+    match bit {
+        PBit::N => cpu.p |= (set << 7),
+        PBit::V => cpu.p |= (set << 6),
+        PBit::B => cpu.p |= (set << 4),
+        PBit::I => cpu.p |= (set << 2),
+        PBit::Z => cpu.p |= (set << 1),
+        PBit::C => cpu.p |= set,
+    }
+}
 
 fn emulate(cart: &Cartridge) {
 
@@ -38,15 +131,18 @@ fn emulate(cart: &Cartridge) {
 
     loop {
         
-        let opcode = read_mem(cpu.pc, &wram, cart);
+        let (opcode, byte2, byte3) = read_three_bytes(cpu.pc, &wram, cart);
+        
         match opcode {
-            opc::ADC_ABS => {},
+            opc::ADC_ABS  => {
+                // let (sum, carry) = read_mem()
+            },
             opc::ADC_ABSX => {}, 
             opc::ADC_ABSY => {}, 
-            opc::ADC_IMM => {}, 
+            opc::ADC_IMM  => {}, 
             opc::ADC_INDX => {}, 
             opc::ADC_INDY => {}, 
-            opc::ADC_ZPG => {}, 
+            opc::ADC_ZPG  => {}, 
             opc::ADC_ZPGX => {}, 
             
             opc::AND_ABS => {}, 
@@ -228,47 +324,4 @@ fn emulate(cart: &Cartridge) {
 
     }
     
-}
-
-fn read_mem(addr: u16, wram: &[u8], cart: &Cartridge) -> u8 {
-    match addr {
-        0x0000..=0x1FFF => wram[(addr % 0x800) as usize],
-        0x2000..=0x3FFF => wram[(0x2000 + (addr % 0x8)) as usize],
-        0x4000..=0x401F => wram[addr as usize],
-        0x4020..=0x5FFF => wram[addr as usize],
-        0x6000..=0x7FFF => wram[addr as usize],
-        0x8000..=0xBFFF => cart.prg_rom[addr as usize],
-        0xC000..=0xFFFF => cart.prg_rom[(addr - 0x4000) as usize],
-    }
-}
-
-fn write_mem(addr: u16, val: u8, wram: &mut [u8]) {
-    if addr < 0x2000 {
-        wram[(addr % 0x800) as usize] = val;
-    }
-}
-
-fn main() {
-
-    let ines_data = fs::read("rom.nes").expect("Failed to read rom");
-
-    if ines_data.len() < 16 {
-        panic!();
-    }
-    if &ines_data[0..4] != b"NES\x1A" {
-        panic!();
-    }
-    
-    let prg_start: usize = 16;
-    let prg_end = prg_start + (ines_data[4] as usize) * 16384;
-    let chr_end = prg_end + (ines_data[5] as usize) * 8192;
-
-    let cart = Cartridge {
-        prg_rom: ines_data[prg_start..prg_end].to_vec(),
-        chr_rom: ines_data[prg_end..chr_end].to_vec(),
-        mapper: (ines_data[7] & 0xF0) | (ines_data[6] >> 4),
-    };
-
-    emulate(&cart);
-
 }
